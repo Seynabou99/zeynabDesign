cbc5ccb2c54d4170b1ace4261ae9409f3876d4b2
Seynabou99 <53898095+Seynabou99@users.noreply.github.com>
2019-09-17 17:51:16 +0200

    Update devinette.js

--- devinette.js
+++ devinette.js
@@ -4,8 +4,8 @@ Activité : jeu de devinette
 
 console.log("Bienvenue dans ce jeu de devinette !");
 
-// Cette ligne génère aléatoirement un nombre entre 1 et 9
-var solution = Math.floor(Math.random() * 9) + 1;
+// Cette ligne génère aléatoirement un nombre entre 0 et 9
+var solution = Math.floor(Math.random() * 9) + 0;
 
 // Décommentez temporairement cette ligne pour mieux vérifier le programme
 //console.log("(La solution est " + solution + ")");
@@ -19,7 +19,7 @@ var essaiMax=3; //nombre maximum d'essai
  
 loopWhile:
 do {             
-            proposition = Number(prompt("Entrez un nombre entre 1 et 9:"));
+            proposition = Number(prompt("Entrez un nombre entre 0 et 9:"));
 
             if (proposition<solution)
             {
@@ -29,12 +29,12 @@ do {
             {
                 console.log(proposition+" est trop grand.");
             }
-            else if (Number.isNaN(proposition)||proposition<1||proposition>9){
+            else if (Number.isNaN(proposition)||proposition<0||proposition>9){
                 alert("Attention ! Veuillez entrer un nombre.");
                             
             }
-            else if (proposition<1||proposition>9){
-                alert("Attention ! Veuillez entrer un nombre compris entre 1 et 9.");
+            else if (proposition<0||proposition>9){
+                alert("Attention ! Veuillez entrer un nombre compris entre 0 et 9.");
             }
 
     essai++;
